e00625e2cae70386a9f49a227bd517fb49aea582
Vivek Raman <vr.ac4bf@live.com>
2023-09-28 15:35:11 +0530

    Add base path

--- template/src/main/java/dev/vivekraman/module/controller/TestController.java
+++ template/src/main/java/dev/vivekraman/module/controller/TestController.java
@@ -1,13 +1,16 @@
 package dev.vivekraman.module.controller;
 
+import dev.vivekraman.module.config.Constants;
 import dev.vivekraman.monolith.model.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 
 @RestController
+@RequestMapping(Constants.MODULE_NAME)
 @RequiredArgsConstructor
 public class TestController {
   private final Scheduler scheduler;
