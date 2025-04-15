da72a017a43bcfb4353ff50a99173f424ca6c283
Vivek Raman <vr.ac4bf@live.com>
2025-04-15 14:23:10 -0700

    Implement template setup script

--- setup-template.py
+++ setup-template.py
@@ -1,9 +1,9 @@
 import argparse
 import os
 import re
-import subprocess
 
 target_version = '1.0-rc1'
+spring_boot_version = '3.1.4'
 
 def parse_arguments():
   parser = argparse.ArgumentParser(description='Backend module setup template')
@@ -50,7 +50,7 @@ def do_fnr(args, filepath, find_text, replace_text):
       if not args.dry_run:
         with open(filepath, 'w', encoding='utf-8') as f:
           f.write(modified_content)
-      print(f'{"[DRY RUN] Replaces" if args.dry_run else "Replaced"} "{match.group(0)}" with "{replace_text}" at {filepath}')
+      print(f'{"[DRY RUN] Replaces" if args.dry_run else "[*] Replaced"} "{match.group(0)}" with "{replace_text}" at {filepath}')
     else:
       print(f'No changes needed in {filepath}')
   except Exception as e:
@@ -69,30 +69,37 @@ def find_and_replace(args, path, find_text, replace_text):
 
 
 def rename(args, orig_path, new_name):
-  if not os.path.exists(orig_path):
-    print(f"Path does not exist: {orig_path}")
-    return False
-
   dir_path = os.path.dirname(orig_path)
   new_path = os.path.join(dir_path, new_name)
 
   if os.path.exists(new_path):
-    print(f"Cannot rename: target already exists: {new_path}")
-    return False
+    os.removedirs(new_path)
 
   if not args.dry_run:
-    os.rename(orig_path, new_path)
-  print(f"{'[DRY RUN] Renames' if args.dry_run else 'Renamed'} {orig_path} to {new_path}")
-  return True
+    os.renames(orig_path, new_path)
+  print(f"{'[DRY RUN] Renames' if args.dry_run else '[*] Renamed'} {orig_path} to {new_path}")
 
 
 def main():
   args = parse_arguments()
+  if not args.dry_run:
+    print('WARNING: --dry-run is not set. Files will be updated. Are you sure you want to continue?')
+    print(f'Module name: {args.name}')
+    print(f'Module name (lowerCamelCase): {get_lower_camel_case(args.name)}')
+    print(f'Module name (UpperCamelCase): {get_upper_camel_case(args.name)}')
+    print(f'Module name (directory/name): {get_directory_name(args.name)}')
+    print(f'Module name (package.name): {get_package_name(args.name)}')
+    print('')
+    go_ahead = input(f'Continue? (y/N) ')
+    if not go_ahead or go_ahead.lower() != 'y':
+      print('Aborting.')
+      return
 
   print(f'Updating version numbers...')
   find_and_replace(args, './module-app/pom.xml', '<version>.+</version>', f'<version>{target_version}</version>')
   find_and_replace(args, './template/pom.xml', '<version>.+</version>', f'<version>{target_version}</version>')
   find_and_replace(args, './pom.xml', '<version>.+</version>', f'<version>{target_version}</version>')
+  find_and_replace(args, './pom.xml', '\t\t<version>.+</version>', f'    <version>{spring_boot_version}</version>')
 
   print(f'Updating parent artifact IDs...')
   find_and_replace(args, './module-app/pom.xml', '<artifactId>backend-module-template</artifactId>', f'<artifactId>backend-module-{args.name}</artifactId>')
@@ -133,13 +140,15 @@ def main():
 
   print(f'Renaming class directories...')
   rename(args, './module-app/src/main/java/dev/vivekraman/module', get_directory_name(args.name))
+  rename(args, './module-app/src/test/java/dev/vivekraman/module', get_directory_name(args.name))
+  rename(args, './template/src/main/java/dev/vivekraman/module', get_directory_name(args.name))
 
-  print(f'Installing...')
-  if not args.dry_run:
-    print('Running mvn clean install -U...')
-    subprocess.run(['mvn', 'clean', 'install', '-U'], check=True)
-  else:
-    print('[DRY RUN] Would run: mvn clean install -U')
+  print(f'Renaming resources...')
+  rename(args, './module-app/src/main/resources/application-module.properties', f'application-{args.name}.properties')
+
+  print(f'Renaming modules...')
+  rename(args, './template', f'{args.name}')
+  rename(args, './module-app', f'{args.name}-app')
 
   if args.dry_run:
     print('Completed dry run. Remove --dry-run from the arguments to apply these changes.')
